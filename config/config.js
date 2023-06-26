092b42533206951a10d7d97d1f7d393ce2c88063
Wayne <waynefenwick@gmail.com>
2023-06-26 05:47:32 -0500

    Discarded changes

--- config/config.js
+++ config/config.js
@@ -10,8 +10,7 @@ const sequelize = process.env.JAWSDB_URL
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
       decimalNumbers: true,
